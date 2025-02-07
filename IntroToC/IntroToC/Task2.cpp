1952bd621a9ded0552bdc10ab3cc83df077eeaf4
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-02-07 14:37:13 +0200

    Add task 2

---
+++ IntroToC/IntroToC/Task2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main(void) {
+	printf("I'm .....Simona.....Nikolaeva.....Ognyanova \n \n");
+
+	printf("My name is: \n");
+	printf("Simona \n");
+	printf("Nikolaeva \n");
+	printf("Ognyanova \n \n");
+
+	printf("My name is: \n \n");
+	printf("Simona \n \n");
+	printf("Nikolaeva \n \n");
+
+}
