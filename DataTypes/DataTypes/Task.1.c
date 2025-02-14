7ee55676b4db7e8306c3e2d244e853d983fffc1f
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-02-14 14:35:31 +0200

    Add task 1 for data types

---
+++ DataTypes/DataTypes/Task.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(void) {
+	int a, b;
+	float c, d;
+
+	a = 10;
+	b = 8;
+	c = 3.2;
+	d = 5.4;
+
+	printf("%i \n", a / b);
+	printf("%i \n", a % b);
+	printf("%f \n", c * d);
+	printf("%f \n", c - d);
+}
