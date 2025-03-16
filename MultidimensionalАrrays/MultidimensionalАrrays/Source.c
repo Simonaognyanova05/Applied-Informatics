4d196d72992b98a4ac4e0d51a13d5aa61183d5bd
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-16 21:11:12 +0200

    Add task 1

---
+++ MultidimensionalАrrays/MultidimensionalАrrays/Source.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+	int A[2][3] = { {10, 20, 30}, {40, 50, 60} };
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%i ", A[i][j]);
+		}
+		printf("\n");
+	}
+}
