94bf806f2df6b5500199f186aab390e4bc5c881a
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-21 12:21:24 +0200

    Add exam tasks

---
+++ Exam/Exam/Source.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void main() {
+    int patnici[2][6]; 
+    int obshtoPatnici = 0; 
+
+    for (int vlak = 0; vlak < 2; vlak++) {
+        printf("Vlak %i:\n", vlak + 1);
+        for (int gara = 0; gara < 6; gara++) {
+            printf("Broi patnici na gara %i: ", gara + 1);
+            scanf_s("%i", &patnici[vlak][gara]);
+            obshtoPatnici += patnici[vlak][gara];
+        }
+    }
+
+    int sredno = obshtoPatnici / (2 * 6);
+
+    printf("Srednoaritmetichen broi patnici: %i \n", sredno);
+}
---
+++ Exam/Task.2/Source.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+void main() {
+    int patnici[2][5]; 
+    int maxGara[2], minGara[2];
+    int maxPatnici[2], minPatnici[2]; 
+
+    for (int vlak = 0; vlak < 2; vlak++) {
+        printf("Vlak % i:\n", vlak + 1);
+        for (int gara = 0; gara < 5; gara++) {
+            printf("Broi panici na gara %i: ", gara + 1);
+            scanf_s("%i", &patnici[vlak][gara]);
+        }
+    }
+
+    for (int vlak = 0; vlak < 2; vlak++) {
+        maxGara[vlak] = 0;
+        minGara[vlak] = 0;
+        maxPatnici[vlak] = patnici[vlak][0];
+        minPatnici[vlak] = patnici[vlak][0];
+
+        for (int gara = 1; gara < 5; gara++) {
+            if (patnici[vlak][gara] > maxPatnici[vlak]) {
+                maxPatnici[vlak] = patnici[vlak][gara];
+                maxGara[vlak] = gara;
+            }
+            if (patnici[vlak][gara] < minPatnici[vlak]) {
+                minPatnici[vlak] = patnici[vlak][gara];
+                minGara[vlak] = gara;
+            }
+        }
+    }
+
+    for (int vlak = 0; vlak < 2; vlak++) {
+        printf("Vlak %i:\n", vlak + 1);
+        printf("Nai mnogo patnici na gara %i: %i\n", maxGara[vlak] + 1, maxPatnici[vlak]);
+        printf("Nai malko patnici na gara %i: %i\n", minGara[vlak] + 1, minPatnici[vlak]);
+    }
+}
