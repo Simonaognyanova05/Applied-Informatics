8bb20c29e0822e657b35c3ea207691953054968a
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-24 22:16:15 +0200

    Change task 1 with one function

--- CourseProject/CourseProject/Source.c
+++ CourseProject/CourseProject/Source.c
@@ -1,46 +1,19 @@
 ﻿#include <stdio.h>
 
-void getInfo() {
+void main() {
     printf("Zadanie: Da se sastavi blokova shema na algoritam i da se napishe programa za obedinqvane na 2 masiwa A[8] i B[8]. Elementite na polucheniq masiv C[12] da se sortirat i otpechatat vav vazhodyasht red. \n");
     printf("Simona Ognyanova, 471224015 \n \n");
-}
-
-void initArray(int array[], int n) {
-    for (int i = 0; i < n; i++) {
-        scanf_s("%i", &array[i]);
-    }
-}
-void sort(int a[], int n)
-{
-    int i, j, temp;
-    for (i = 0; i < n - 1; ++i)
-    {
-        for (j = i + 1; j < n; ++j)
-        {
-            if (a[i] > a[j]) {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }       
-    }
-}
-
-void printArray(int array[]) {
-    printf("Sorted array: ");
-    for (int i = 0; i < 12; i++) {
-        printf("%i ", array[i]);
-    }
-}
-void main() {
-    getInfo();
     int A[4], B[8], C[12];
 
     printf("Enter the elements of the first array: \n");
-    initArray(A, 4);
+    for (int i = 0; i < 4; i++) {
+        scanf_s("%i", &A[i]);
+    }
 
     printf("Enter the elements of the second array: \n");
-    initArray(B, 8);
+    for (int i = 0; i < 8; i++) {
+        scanf_s("%i", &B[i]);
+    }
 
     for (int i = 0; i < 4; i++) {
         C[i] = A[i];
@@ -50,6 +23,20 @@ void main() {
         C[4 + i] = B[i];
     }
 
-    sort(C, 12);
-    printArray(C);
+    int temp;
+    for (int i = 0; i < 12 - 1; ++i)
+    {
+        for (int j = i + 1; j < 12; ++j)
+        {
+            if (C[i] > C[j]) {
+                temp = C[i];
+                C[i] = C[j];
+                C[j] = temp;
+            }
+        }
+    }
+    printf("Sorted array: ");
+    for (int i = 0; i < 12; i++) {
+        printf("%i ", C[i]);
+    }
 }
