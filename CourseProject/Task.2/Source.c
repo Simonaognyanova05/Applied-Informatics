e0609421394a66b58ea9891cd18a59654154b3f9
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-26 17:51:00 +0200

    Add proccess array function

--- CourseProject/Task.2/Source.c
+++ CourseProject/Task.2/Source.c
@@ -1,40 +1,48 @@
 #include <stdio.h>
+
 void inputArray(int array[][3]) {
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			printf("Element [%i][%i]: ", i, j);
-			scanf_s("%i", &array[i][j]);
-		}
-	}
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Element [%i][%i]: ", i, j);
+            scanf_s("%i", &array[i][j]);
+        }
+    }
 }
 
 void findSumOfElements(int arr1[][3], int arr2[][3], int resultArr[][3]) {
-	
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			resultArr[i][j] = arr1[i][j] + arr2[i][j];
-		}
-	}
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            resultArr[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
 }
 
 void printArray(int array[][3]) {
-	printf("Novopoluchen masiv: \n");
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			printf("%i ", array[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%i ", array[i][j]);
+        }
+        printf("\n");
+    }
 }
+
+void processArrays() {
+    int arr1[2][3], arr2[2][3], resultArr[2][3];
+
+    printf("Vavedete elementite na masiv 1: \n");
+    inputArray(arr1);
+
+    printf("Vavedete elementite na masiv 2: \n");
+    inputArray(arr2);
+
+    findSumOfElements(arr1, arr2, resultArr);
+    printf("Novopoluchen masiv: \n");
+    printArray(resultArr);
+}
+
 void main() {
-	int arr1[2][3], arr2[2][3], resultArr[2][3];
-
-	printf("Vavedete elementite na masiv 1: \n");
-	inputArray(arr1);
-
-	printf("Vavedete elementite na masiv 2: \n");
-	inputArray(arr2);
-
-	findSumOfElements(arr1, arr2, resultArr);
-	printArray(resultArr);
+    for (int i = 0; i < 2; i++) {
+        printf("Vavejdane na masivi nomer (%i):\n", i + 1);
+        processArrays();
+    }
 }
