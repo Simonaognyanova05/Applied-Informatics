0d0e5b3c58f97a2b1f9f8a69e6bdf9c5e94ad9be
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-26 17:22:06 +0200

    Add function for inputing array

--- CourseProject/Task.2/Source.c
+++ CourseProject/Task.2/Source.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
+void inputArray(int array[][3]) {
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("Element [%i][%i]: ", i, j);
+			scanf_s("%i", &array[i][j]);
+		}
+	}
+}
 void main() {
 	int arr1[2][3], arr2[2][3], resultArr[2][3];
 
 	printf("Vavedete elementite na masiv 1: \n");
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			printf("Element [%i][%i]: ", i, j);
-			scanf_s("%i", &arr1[i][j]);
-		}
-	}
+	inputArray(arr1);
 
 	printf("Vavedete elementite na masiv 2: \n");
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			printf("Element [%i][%i]: ", i, j);
-			scanf_s("%i", &arr2[i][j]);
-		}
-	}
+	inputArray(arr2);
 
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 3; j++) {
