6c02ea3f9f64758e4f94d402cd4974c46d41689b
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-04-27 14:00:19 +0300

    Add string initialization

---
+++ Strings/Strings/Source.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+	char A[] = { "Hello!" };
+	char B[] = { 'H', 'I', '!'};
+	char C[7] = { "Hello!" };
+	char D[6] = { "Hello!" };
+
+
+	printf("%s", A);
+	printf("%s", D);
+
+}
