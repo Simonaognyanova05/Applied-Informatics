9c2c03fe4a5d69f2eaab014e2fa0a6a3fabb4d05
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-05-09 12:45:49 +0300

    Add one more function to exam task

--- Exam - 2/Exam - 2/Source.c
+++ Exam - 2/Exam - 2/Source.c
@@ -1,5 +1,15 @@
 ﻿#include <stdio.h>
 
+void inputArray(double timeArr[3][3]) {
+    printf("Vavedete vremeto za wsyaka danna platka vav vseki ceh:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("Danna platka %d:\n", i + 1);
+        for (int j = 0; j < 3; j++) {
+            printf("Ceh %d: ", j + 1);
+            scanf_s("%lf", &timeArr[i][j]);
+        }
+    }
+}
 double calculateTime(double timeArr[3][3]) {
     double time = 0.0;
 
@@ -14,17 +24,7 @@ double calculateTime(double timeArr[3][3]) {
 
 void main() {
     double timeArr[3][3];
-
-    printf("Vavedete vremeto za wsyaka danna platka vav vseki ceh:\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Danna platka %d:\n", i + 1);
-        for (int j = 0; j < 3; j++) {
-            printf("Ceh %d: ", j + 1);
-            scanf_s("%lf", &timeArr[i][j]);
-        }
-    }
-
+    inputArray(timeArr);
     double totalTime = calculateTime(timeArr);
-
     printf("\nObshtoto vreme, neobhodimo za proizvodstvo: %.2f chasa\n", totalTime);
 }
